ddc8647b91499cd99b59bd9ce60d42798023b95c
rofayels <adamrofayel@gmail.com>
2025-03-25 08:28:57 +0500

    feat:responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { Chat } from "@/components/chat";
 
 export default function Home() {
   return (
-    <div className="h-screen w-[90%] sm:w-[60%] relative mx-auto items-center !pb-4 flex flex-col">
-      <h1 className="italic text-4xl fraunces w-full flex justify-start pt-6 sm:fixed left-8 -z-10 select-none">
+    <div className="h-screen w-[97%] sm:w-[60%] relative mx-auto items-center !pb-4 flex flex-col">
+      <h1 className="italic text-4xl fraunces w-full flex justify-start pt-6 sm:fixed left-8 -z-10 select-none sm:px-0 px-4">
         Raya
       </h1>
       {/* <AppSidebar /> */}
