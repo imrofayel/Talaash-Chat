bfa7d2bb7cfc4f1d4d947e69b8c0804127021018
rofayels <adamrofayel@gmail.com>
2025-03-25 09:47:25 +0500

    style: add bottom padding to header for improved spacing

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Chat } from "@/components/chat";
 export default function Home() {
   return (
     <div className="h-screen w-[97%] sm:w-[60%] relative mx-auto items-center !pb-4 flex flex-col">
-      <h1 className="italic text-4xl fraunces w-full flex justify-start pt-6 sm:fixed left-8 -z-10 select-none sm:px-0 px-4">
+      <h1 className="italic text-4xl fraunces w-full flex justify-start pt-6 sm:fixed left-8 -z-10 select-none sm:px-0 px-4 pb-3">
         Raya
       </h1>
       {/* <AppSidebar /> */}
